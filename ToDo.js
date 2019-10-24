50bd941d46023d7e062259e81137eda7a53dab73
jjyoon7 <jay.yoon7@gmail.com>
2019-10-24 11:18:07 +0200

    update loadToDos function

--- ToDo.js
+++ ToDo.js
@@ -5,6 +5,11 @@ const toDos_LS = "toDos";
 
 function loadToDos(){
     const toDos = localStorage.getItem(toDos_LS);
+    if(toDos === null){
+        //if there are no todos, then get the todos
+    } else {
+        //show the todos
+    }
 }
 
 function init() {
