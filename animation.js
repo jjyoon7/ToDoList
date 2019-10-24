997e9711c56f7b7bbb2e6a1efabcd4f51f8c9dcc
jjyoon7 <jay.yoon7@gmail.com>
2019-10-24 20:09:20 +0200

    update html structure and add h4 to make it more clear for the user what they need to do and they have done

--- animation.js
+++ animation.js
@@ -3,8 +3,8 @@ const tl = new TimelineMax();
 function animate() {
     tl.addLabel('start')
     .from('.toDoBody', 1, { autoAlpha: 0, y:50, ease: Back.easeout }, 'start')
-    .from('.toDoList', 0.8, {autoAlpha: 0, x:40, ease:Back.easeOut}, 'start+=0.7')
-    .from('.doneDoList', 0.8, {autoAlpha: 0, x:-40, ease:Back.easeOut}, 'start+=0.7')
+    .from('.toDoLeftDiv', 0.8, {autoAlpha: 0, x:40, ease:Back.easeOut}, 'start+=0.7')
+    .from('.doneDiv', 0.8, {autoAlpha: 0, x:-40, ease:Back.easeOut}, 'start+=0.7')
 }
 
 
