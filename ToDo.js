39796e9d8173372c7e19b7ffc580c82cb3de4923
jjyoon7 <jay.yoon7@gmail.com>
2019-10-24 11:19:14 +0200

    update if statement in loadToDos function

--- ToDo.js
+++ ToDo.js
@@ -5,9 +5,7 @@ const toDos_LS = "toDos";
 
 function loadToDos(){
     const toDos = localStorage.getItem(toDos_LS);
-    if(toDos === null){
-        //if there are no todos, then get the todos
-    } else {
+    if(toDos !== null){
         //show the todos
     }
 }
