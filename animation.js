4ab935b79f8b590c5f83a1efded87e1df3a2f37a
jjyoon7 <jay.yoon7@gmail.com>
2019-10-24 19:28:32 +0200

    add animation to toDoCard and toDoList and doneDoList

--- animation.js
+++ animation.js
@@ -1,5 +1,15 @@
+const tl = new TimelineMax();
+
+function animate() {
+    tl.addLabel('start')
+    .from('.toDoBody', 1, { autoAlpha: 0, y:50, ease: Back.easeout }, 'start')
+    .from('.toDoList', 0.8, {autoAlpha: 0, x:40, ease:Back.easeOut}, 'start+=0.7')
+    .from('.doneDoList', 0.8, {autoAlpha: 0, x:-40, ease:Back.easeOut}, 'start+=0.7')
+}
+
+
 function init() {
-    animte();
+    animate();
 }
 
 init();
